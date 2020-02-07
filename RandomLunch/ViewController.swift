c3cb42043893a318b18b3958634d863ccea2d777
zino <minzino@hanmail.net>
2020-02-07 22:47:46 +0900

    last commit

--- RandomLunch/ViewController.swift
+++ RandomLunch/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let names=["짜장면","스파게티","삼겹살","부대찌게","쌀국수","햄버거"]
+    
+    @IBOutlet weak var la_lunch: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func btnClick(_ sender: Any) {
+        getLunch()
+    }
+    
+    func getLunch(){
+        let num = Int.random(in: 0...5)
+        la_lunch.text = names[num]
+        
+    }
 }
 
